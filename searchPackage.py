69876a5d8035b91ac7f0d0f31e673be70894dc64
Aaron Wang <z5308498@ad.unsw.edu.au>
2023-06-25 19:53:22 +1000

    feature: added initial search feature that displays packages based on LVR and IR

---
+++ searchPackage.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from packages import Packages # placeholder for retrieving package info file
+
+app = Flask(__name__)
+
+@app.route("/search",  methods=['POST'])
+def searchPackage():
+
+    # retrieve package list
+    packages = Packages.getList() # placeholder for retrieving package info function
+    
+    # get user input for estimated property value and borrowing amount
+    estimated_value = float(request.form.get('estimated_value'))
+    borrowing_amount = float(request.form.get('borrowing_amount'))
+
+    # calculate and store loan-to-value ratio (LVR)
+    lvr = borrowing_amount / estimated_value
+
+    # exclude loan packages based on their maximum LVR
+    lvrList = [p for p in packages if p['max_lvr'] >= lvr]
+
+    # sort loan packages by their interest rate (ascending)
+    irList = sorted(lvrList, key=lambda x: x['interest_rate'])
+
+    return irList
