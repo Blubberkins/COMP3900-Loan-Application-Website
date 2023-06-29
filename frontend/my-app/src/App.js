303e6e13a610afcc2dd0972794e5313059e8cb96
Joseph Day <z5207816@ad.unsw.edu.au>
2023-06-29 15:41:15 +1000

    feature: Added search and recommendation system

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -4,18 +4,26 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import OverviewPage from './pages/OverviewPage';
+import HomeLoanPage from './pages/HomeLoanPage';
+import LoanResultsPage from './pages/LoanResultsPage';
 
 function App() {
-  return (
-    <div className="">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/overview" element={<OverviewPage />} />
+                    <Route path="/edit-package/:packageId" element={<div />} />
+                    <Route path="/home-loan" element={<HomeLoanPage/>} />
+                    <Route path="/loan-results" element={<LoanResultsPage />} />
+                    <Route path="/appointment" element={<div />}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/my-app/src/pages/HomeLoanPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeLoanForm from '../components/HomeLoanForm';
+
+function HomeLoanPage() {
+    return (
+        <div className="homeloan-page flex flex-col items-center justify-center h-screen bg-blue-50">
+            <HomeLoanForm />
+        </div>
+    );
+}
+
+export default HomeLoanPage;
